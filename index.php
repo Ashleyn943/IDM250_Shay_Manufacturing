a30dcdd530d3b09ef9c082a6004806a3b26172be
Raymond Gonzalez <rg938@drexel.edu>
2026-02-25 10:02:53 -0500

    Add MPL/order APIs, update DB dump & UI fixes

--- index.php
+++ index.php
@@ -18,9 +18,6 @@
     <title>Table Practice</title>
 </head>
 <body>
-<<<<<<< Updated upstream
-    <h1>Products</h1>
-=======
 
     <div class="dashboard-container">
     <h1>Dashboard</h1>
@@ -62,7 +59,6 @@
 
     <h1>Products</h1>
         <a href="APIs/product-new.php" class="btn">Add New Product</a>
->>>>>>> Stashed changes
         <table class="data_tb">
             <tr>
                 <th>ID</th>
@@ -82,61 +78,25 @@
                     if($row = mysqli_num_rows($result)){
                         foreach($result as $row){
                             echo "<tr>";
-                            echo "<td>" . htmlspecialchars($row['ficha']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['ficha'] ?? '') . "</td>";
                             echo "<td>" . htmlspecialchars($row['sku']) . "</td>";
                             echo "<td>" . htmlspecialchars($row['description']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['uom_primary']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['piece_count']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['length_inches']) . "in </td>";
-                            echo "<td>" . htmlspecialchars($row['width_inches']) . "in </td>";
-                            echo "<td>" . htmlspecialchars($row['height_inches']) . "in </td>";
-                            echo "<td>" . htmlspecialchars($row['weight_lbs']) . "lb </td>";
-                            echo "<td>" . htmlspecialchars($row['assembly']) . "</td>";
-                            echo "<td> $" . htmlspecialchars($row['rate']) . "</td>";
-                            echo "<td> <a href='edit.php?=" . htmlspecialchars($row['ID']) . "' class=''>Edit</a></td>";
+                            echo "<td>" . htmlspecialchars($row['uom_primary'] ?? '') . "</td>";
+                            echo "<td>" . htmlspecialchars($row['piece_count'] ?? 0) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['length_inches'] ?? 0) . " in </td>";
+                            echo "<td>" . htmlspecialchars($row['width_inches'] ?? 0) . " in </td>";
+                            echo "<td>" . htmlspecialchars($row['height_inches'] ?? 0) . " in </td>";
+                            echo "<td>" . htmlspecialchars($row['weight_lbs'] ?? 0) . " lb </td>";
+                            echo "<td>" . htmlspecialchars($row['assembly'] ?? '') . "</td>";
+                            echo "<td> $" . htmlspecialchars($row['rate'] ?? 0) . "</td>";
+                            echo "<td> <a href='APIs/product-update.php?id=" . htmlspecialchars($row['id'] ?? 0) . "' class=''>Edit</a>
+                            |
+                            <a href='APIs/product-delete.php?id=" . htmlspecialchars($row['id'] ?? 0) . "' class=''>Delete</a></td>";
                             echo "</tr>";
                         }
                     };
             ?>
         </table>
     <br><br>
-    <h1>Current Stock</h1>
-        <table class="data_tb">
-            <tr>
-                <th>Select</th>
-                <th>Order Number</th>
-                <th>Shipment Number</th>
-                <th>Bill Number</th>
-                <th>Purchase Number</th>
-                <th>Unit Number</th>
-                <th>Ficha</th>
-                <th>Description 1</th>
-                <th>Description 2</th>
-                <th>Quantity</th>
-                <th>Quantity Unit</th>
-                <th>Footage Quantity</th>
-            </tr>
-            <?php
-                $result = mysqli_query($connection, "SELECT * FROM inventory_item_info");
-                    if($row = mysqli_num_rows($result)){
-                        foreach($result as $row){
-                            echo "<tr>";
-                            echo "<td> <input type='checkbox' value='selected'> </td>";
-                            echo "<td>" . htmlspecialchars($row['order_numb']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['shipment_numb']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['bill_numb']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['purchase_numb']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['unit_numb']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['ficha']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['description_1']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['description_2']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['quantity']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['quantity_unit']) . "</td>";
-                            echo "<td>" . htmlspecialchars($row['footage_quantity']) . "</td>";
-                            echo "</tr>";
-                        }   
-                    }
-            ?>
-        </table>
 </body>
 </html>
